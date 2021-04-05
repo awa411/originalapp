f6dcff51269e3ee71ca8e9ccd101548cc2d0227a
awa411 <ad1117wi@gmail.com>
2021-04-05 12:10:03 +0900

    top-regi-new views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
 
+         validates :nickname, presence: true
          validates_format_of :password, with: PASSWORD_REGEX
-
-         belongs_to :user
+         has_many :articles
          #has_many   :purchases
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
-  resources :articles, only: [:index] do
+  resources :articles, only: [:index, :new] do
     collection do
       get "search"
     end
