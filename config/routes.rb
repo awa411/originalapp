a8a962df4653b24ec20a2d09316bc807d8b3bf31
awa411 <ad1117wi@gmail.com>
2021-04-07 00:41:17 +0900

    article-show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
-  resources :articles, only: [:index, :new, :create] do
+  resources :articles, only: [:index, :new, :create, :show, :edit, :update] do
     collection do
       get "search"
     end
   end
+  resources :users, only: [:show]
 end
