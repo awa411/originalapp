a3112d0e3023ebddd34069cea84d861b0666c384
awa411 <ad1117wi@gmail.com>
2021-04-11 01:08:21 +0900

    model test

---
+++ spec/factories/article.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :article do
+    title       {Faker::Name.initials(number: 2)}
+    category_id {rand(1..17)}
+    text        {Faker::Lorem.sentence}
+
+    association :user
+
+    after(:build) do |article|
+      article.image.attach(io: File.open('app/assets/images/paper_fibers.png'), filename: 'test_image.jpg')
+    end
+  end
+end
+
