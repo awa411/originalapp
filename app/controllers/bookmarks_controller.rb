13be257e9d76437cb5ab9bbb58620ba881ac28b2
awa411 <ad1117wi@gmail.com>
2021-05-25 12:05:35 +0900

    bookmark-asyn

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,13 @@
 class BookmarksController < ApplicationController
   def create
-    article = Article.find(params[:article_id])
-    current_user.bookmark(article)
-    redirect_back fallback_location: root_path, success: 'ブックマークしました'
+    @article = Article.find(params[:article_id])
+    current_user.bookmark(@article)
+    # redirect_back fallback_location: root_path, success: 'ブックマークしました'
   end
 
   def destroy
-    article = current_user.bookmarks.find_by(id: params[:id]).article
-    current_user.unbookmark(article)
-    redirect_back fallback_location: root_path, success: 'ブックマークを外しました'
+    @article = current_user.bookmarks.find_by(id: params[:id]).article
+    current_user.unbookmark(@article)
+    # redirect_back fallback_location: root_path, success: 'ブックマークを外しました'
   end
 end
